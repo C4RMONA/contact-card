0332428b08c0240913564b91284804b36e58f3fd
chris <christiancarmona3@gmail.com>
2022-08-22 13:32:10 -0500

    added webpack

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,8 +1,18 @@
+// Import modules
 import "./form";
 import "./submit";
+
+//import css files
+import "../css/index.css";
+
+//import images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
+
+//import bootstrap
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 window.addEventListener('load', function () {
     document.getElementById('logo').src = Logo;
